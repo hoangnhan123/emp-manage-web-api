0528c9416567d37229a5e81ed40ceac56eb3a00e
nhannh1201 <hoangnhan123vnz@gmail.com>
2022-09-08 21:35:50 +0700

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @Get('me')
-    getMe() {
-        return 'user info';
-    }
+ 
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
 }
